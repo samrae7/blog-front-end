9cc06a1da00baae93052051dc148d4784bca4a79
Sam Rae <samuelrae@gmail.com>
2018-08-26 21:57:21 +0100

    add postCard component

---
+++ src/components/postCard.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+PostCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+function PostCard(props) {
+  const { classes, post } = props;
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary">
+          {new Date(post.dateCreated).toDateString()}
+        </Typography>
+        <Typography variant="headline" component="h2">
+          {post.title}
+        </Typography>
+        <Typography component="p">
+          {post.body}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Read more</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default withStyles(styles)(PostCard);
