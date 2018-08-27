00af3fc7738cc84d93d9d94281ad2e196ede6d51
Sam Rae <samuelrae@gmail.com>
2018-08-27 21:50:19 +0100

    got drawer and appbar working

--- src/components/ResponsiveDrawer.tsx
+++ src/components/ResponsiveDrawer.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import {withStyles, createStyles} from '@material-ui/core/styles';
-import {Theme} from '@material-ui/core/styles/createMuiTheme';
-import {WithStyles} from '@material-ui/core/styles/withStyles';
+import classNames from 'classnames'
+import { withStyles, createStyles } from '@material-ui/core/styles';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import { WithStyles } from '@material-ui/core/styles/withStyles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
+import {List, ListItem, ListItemText, ListItemIcon} from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import Hidden from '@material-ui/core/Hidden';
 import Divider from '@material-ui/core/Divider';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import InboxIcon from '@material-ui/icons/Inbox';
-import {Route} from 'react-router-dom';
-import {PostsList} from './postsList';
+import { Route } from 'react-router-dom';
+import { PostsList } from './postsList';
 
 const drawerWidth = 240;
 
 const styles = (theme: Theme) => createStyles({
   root: {
     flexGrow: 1,
+  },
+  appFrame: {
+    height: 430,
     zIndex: 1,
     overflow: 'hidden',
     position: 'relative',
@@ -28,49 +33,102 @@ const styles = (theme: Theme) => createStyles({
   },
   appBar: {
     position: 'absolute',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'appBarShift-left': {
     marginLeft: drawerWidth,
-    [theme.breakpoints.up('md')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
   },
-  navIconHide: {
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
+  'appBarShift-right': {
+    marginRight: drawerWidth,
   },
-  toolbar: theme.mixins.toolbar,
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
+  },
   drawerPaper: {
+    position: 'relative',
     width: drawerWidth,
-    [theme.breakpoints.up('md')]: {
-      position: 'relative',
-    },
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
   },
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  'content-left': {
+    marginLeft: -drawerWidth,
+  },
+  'content-right': {
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  'contentShift-left': {
+    marginLeft: 0,
+  },
+  'contentShift-right': {
+    marginRight: 0,
   },
 });
 
 interface DrawerState {
-  mobileOpen: boolean;
+  open: boolean;
 }
 
 class ResponsiveDrawer extends React.Component<WithStyles<typeof styles>, DrawerState> {
   state = {
-    mobileOpen: false,
+    open: false
   };
 
   handleDrawerToggle = () => {
-    this.setState((state: DrawerState) => ({ mobileOpen: !state.mobileOpen }));
+    this.setState((state: DrawerState) => ({ open: !state.open }));
   };
 
-  render(): JSX.Element {
+  render() {
     const { classes, theme } = this.props;
+    const { open } = this.state;
 
     const drawer = (
-      <div>
-        <div className={classes.toolbar} />
+      <Drawer
+        variant="persistent"
+        anchor={"left"}
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={this.handleDrawerToggle}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
         <Divider />
         <List>
           <ListItem>
@@ -79,63 +137,53 @@ class ResponsiveDrawer extends React.Component<WithStyles<typeof styles>, Drawer
             </ListItemIcon>
             <ListItemText primary="Inbox" />
           </ListItem>
+          <Divider />
         </List>
-        <Divider />
-      </div>
+      </Drawer>
     );
 
     return (
       <div className={classes.root}>
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerToggle}
-              className={classes.navIconHide}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" noWrap>
-              Responsive drawer
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Hidden mdUp>
-          <Drawer
-            variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={this.state.mobileOpen}
-            onClose={this.handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
+        <div className={classes.appFrame}>
+          <AppBar
+            className={classNames(classes.appBar, {
+              [classes.appBarShift]: open,
+              [classes[`appBarShift-left`]]: open,
+            })}
           >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden smDown implementation="css">
-          <Drawer
-            variant="persistent"
-            open
-            classes={{
-              paper: classes.drawerPaper,
-            }}
+            <Toolbar disableGutters={!open}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classNames(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" noWrap>
+                Sam's blog
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          {drawer}
+          <main
+            className={classNames(classes.content, classes[`content-left`], {
+              [classes.contentShift]: open,
+              [classes[`contentShift-left`]]: open,
+            })}
           >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          {/* <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography> */}
-          <Route path='/posts' component={PostsList} />
-        </main>
+            <div className={classes.drawerHeader} />
+            <Route path='/posts' component={PostsList} />
+          </main>
+        </div>
       </div>
     );
   }
+
+
+
+
+
 }
 
 export default withStyles(styles, { withTheme: true })(ResponsiveDrawer);
