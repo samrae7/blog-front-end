49c78abaf7e7b480485fdf9a9d5446c2cf42d6e3
Sam Rae <samuelrae@gmail.com>
2018-09-02 09:09:21 +0100

    removed data fetching from postsList

--- src/components/postsList.jsx
+++ src/components/postsList.jsx
@@ -1,31 +1,11 @@
 import * as React from "react";
 import PostCard from './PostCard';
 
-class PostsList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { posts: [] };
-  }
-
-  componentDidMount() {
-    this.getPosts();
-  }
-
-  async getPosts() {
-    const response = await fetch("http://localhost:5000/api/post", {
-      method: "GET"
-    });
-    const data = await response.json();
-    this.setState({
-      posts: data
-    });
-    return data;
-  }
-
+class PostsList extends React.Component<{posts: Post[]}, null> {
   render() {
     return (
       <div>
-        {this.state.posts.map((post, i) => 
+        {this.props.posts.map((post, i) => 
             <PostCard post={post} key={i} />
           )
         }
