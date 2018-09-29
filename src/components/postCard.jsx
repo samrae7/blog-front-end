712b242babf67b84c900915fa0a238b9769b3a74
Sam Rae <samuelrae@gmail.com>
2018-09-29 19:07:05 +0100

    added ink to post page from post card READ MORE button

--- src/components/postCard.jsx
+++ src/components/postCard.jsx
@@ -1,33 +1,33 @@
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 PostCard.propTypes = {
   classes: PropTypes.object.isRequired,
-  post: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired
 };
 
 const styles = {
   card: {
-    minWidth: 275,
+    minWidth: 275
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
   },
   title: {
     marginBottom: 16,
-    fontSize: 14,
+    fontSize: 14
   },
   pos: {
-    marginBottom: 12,
-  },
+    marginBottom: 12
+  }
 };
 
 function PostCard(props) {
@@ -45,7 +45,9 @@ function PostCard(props) {
         <Typography component="p">{post.body}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Read more</Button>
+        <Button href={`/posts/${post.id}`} size="small">
+          Read more
+        </Button>
       </CardActions>
     </Card>
   );
