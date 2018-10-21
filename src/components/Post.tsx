a80b9c0f1e123da850328210a0f8916616ffd719
Sam Rae <samuelrae@gmail.com>
2018-10-21 20:07:16 +0100

    add image to post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,4 @@
-import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import { createStyles, withStyles } from "@material-ui/core/styles";
 import { WithStyles } from "@material-ui/core/styles/withStyles";
@@ -32,6 +30,11 @@ const Post: React.StatelessComponent<IPostProps> = (props: IPostProps) => {
   return (
     <Card className={classes.card}>
       <CardContent>
+        <img
+          src={`https://s3.eu-west-2.amazonaws.com/secret-london-blog/${
+            post.imageId
+          }`}
+        />
         <Typography className={classes.title} color="textSecondary">
           {new Date(post.dateCreated).toDateString()}
         </Typography>
