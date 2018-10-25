e31b8b01a5d9f23063f82a993a5e91731173869a
Sam Rae <samuelrae@gmail.com>
2018-10-25 23:59:39 +0100

    add link to edit post

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,9 +1,12 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
 import { createStyles, withStyles } from "@material-ui/core/styles";
 import { WithStyles } from "@material-ui/core/styles/withStyles";
 import Typography from "@material-ui/core/Typography";
-import * as React from "react";
 import { IPost } from "../types";
 
 const styles = () =>
@@ -47,6 +50,9 @@ const Post: React.StatelessComponent<IPostProps> = (props: IPostProps) => {
         </Typography>
         <Typography component="p">{post.intro}</Typography>
         <Typography component="p">{post.body}</Typography>
+        <Button variant="contained" href={`edit/${post.id}`}>
+          Edit post
+        </Button>
       </CardContent>
     </Card>
   );
