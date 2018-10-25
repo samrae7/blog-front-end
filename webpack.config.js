022adffe126d0c660cfba73f6ec23636a765671a
Sam Rae <samuelrae@gmail.com>
2018-10-25 18:13:18 +0100

    add sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "cheap-module-eval-source-map",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.(tsx?|jsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: ["node_modules"]
       }
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js', '.jsx' ]
+    extensions: [".tsx", ".ts", ".js", ".jsx"]
   }
 };
